2f08e28cbe60994f1c872b7ae1f2a6ada61488f1
MuhammadFandu02 <muhammadfandupratama0225@gmail.com>
2023-05-06 22:54:06 +0700

    TUGAS WEB

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+        public function index()
+        {
+            echo "<h1>Perkenalkan</h1>";
+            echo"Nama saya Muhammad Fandu Pratama
+            Saya tinggal di Depok
+            Olah raga yang saya sukai adalah Berenang";
+        }
+}
